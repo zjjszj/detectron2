1d419aea0f6b89ba458080444116fd0a51a5a356
ygsm <1411605960@qq.com>
2020-05-13 23:24:10 +0800

    use myself dataset

--- detectron2/data/datasets/builtin.py
+++ detectron2/data/datasets/builtin.py
@@ -113,23 +113,23 @@ def register_all_coco(root):
                 os.path.join(root, json_file) if "://" not in json_file else json_file,
                 os.path.join(root, image_root),
             )
-
-    for (
-        prefix,
-        (panoptic_root, panoptic_json, semantic_root),
-    ) in _PREDEFINED_SPLITS_COCO_PANOPTIC.items():
-        prefix_instances = prefix[: -len("_panoptic")]
-        instances_meta = MetadataCatalog.get(prefix_instances)
-        image_root, instances_json = instances_meta.image_root, instances_meta.json_file
-        register_coco_panoptic_separated(
-            prefix,
-            _get_builtin_metadata("coco_panoptic_separated"),
-            image_root,
-            os.path.join(root, panoptic_root),
-            os.path.join(root, panoptic_json),
-            os.path.join(root, semantic_root),
-            instances_json,
-        )
+    ## 
+    # for (
+    #     prefix,
+    #     (panoptic_root, panoptic_json, semantic_root),
+    # ) in _PREDEFINED_SPLITS_COCO_PANOPTIC.items():
+    #     prefix_instances = prefix[: -len("_panoptic")]
+    #     instances_meta = MetadataCatalog.get(prefix_instances)
+    #     image_root, instances_json = instances_meta.image_root, instances_meta.json_file
+    #     register_coco_panoptic_separated(
+    #         prefix,
+    #         _get_builtin_metadata("coco_panoptic_separated"),
+    #         image_root,
+    #         os.path.join(root, panoptic_root),
+    #         os.path.join(root, panoptic_json),
+    #         os.path.join(root, semantic_root),
+    #         instances_json,
+    #     )
 
 
 # ==== Predefined datasets and splits for LVIS ==========
