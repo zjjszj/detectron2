e90d2847347b63f372893c2d8fe721ff908f4bb6
ygsm <1411605960@qq.com>
2020-05-13 12:21:31 +0800

    upload json/jpg and so on

--- detectron2/data/datasets/dd.py
+++ detectron2/data/datasets/dd.py
@@ -1,9 +1,5 @@
 import sys
 import os
-curPath = os.path.abspath(os.path.dirname(__file__))
-print(curPath)
-rootPath = os.path.split(curPath)[0]
-print(rootPath)
 
 
-#print(os.path.isfile('//coco/annotations/instances_train2017.json'))
+print(os.path.isfile('coco/annotations/instances_train2017.json'))
